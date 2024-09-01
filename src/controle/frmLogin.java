9ee56c7d280904d0d9807895ae8221767d000399
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-09-01 20:27:27 -0300

    project has been completed

--- src/controle/frmLogin.java
+++ src/controle/frmLogin.java
@@ -96,6 +96,7 @@ public class frmLogin extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
